8ec38aceb5ca4d1dbe44c991e4fba5b315e70578
vtw <vtiaraw2@gmail.com>
2025-05-29 15:38:17 +0700

    refactor: adjust icon n app name

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>SISPAK-DKPH</title>
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="./assets/img/logo-sispak-dkph.png" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
