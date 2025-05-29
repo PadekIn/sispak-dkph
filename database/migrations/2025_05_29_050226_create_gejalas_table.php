27670933845e6f63f38d9e98f15112b10702ad63
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-05-29 12:30:07 +0700

    feat: penambahan kolom gejala

--- database/migrations/2025_05_29_050226_create_gejalas_table.php
+++ database/migrations/2025_05_29_050226_create_gejalas_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('gejalas', function (Blueprint $table) {
             $table->id();
+            $table->string('kode_gejala');
+            $table->string('nama_gejala');
             $table->timestamps();
         });
     }
