514ad1fc8a2a675e4969e1ca8412c1521479ecb9
vtw <vtiaraw2@gmail.com>
2025-05-29 15:16:51 +0700

    refactor: adjust the logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                         <img src="{{ asset('assets/img/logo-sispak-dkph.png') }}" alt="Logo Sispak DKPH" style="max-width: 50px;">
                     </a>
                 </div>
 
