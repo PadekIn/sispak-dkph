d7d8ce4ed29b287fcd3425b4056cfe49f55ff967
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-05-29 16:48:28 +0700

    feat: penambahan route

--- routes/admin.php
+++ routes/admin.php
@@ -1,2 +1,10 @@
+<?php
+
 use Illuminate\Support\Facades\Route;
 
+
+Route::middleware(['auth', 'isPengguna'])->group(function () {
+
+
+
+});
--- routes/pengguna.php
+++ routes/pengguna.php
@@ -1,3 +1,10 @@
+<?php
+
 use Illuminate\Support\Facades\Route;
 
 
+Route::middleware(['auth', 'isPengguna'])->group(function () {
+
+
+
+});
