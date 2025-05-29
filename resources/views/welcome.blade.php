e5d9bfdb92e210f2687073d37160576ff8109487
dindputr <dprtia15@gmail.com>
2025-05-29 16:35:23 +0700

    feat: add more feat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,40 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-pengguna-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Kuesioner') }}
+        </h2>
+    </x-slot>
 
-        <title>SISPAK-DKPH</title>
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="./assets/img/logo-sispak-dkph.png" type="image/x-icon">
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <!-- Styles / Scripts -->
-
-    </head>
-    <body >
-        <!-- <p class="text-4xl text-blue-500 underline">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-
-        <div class="bg-red-500 p-8 flex gap-4">
-            <div class="bg-green-500 w-3/6">
-                <h1 class="text-2xl font-bold text-white">Welcome to Laravel</h1>
-            </div>
-            <div class="bg-gray-500">
-                <h1 class="text-2xl font-bold text-white">Welcome to Laravel</h1>
-            </div>
-        </div> -->
-
-        <div>
-            <div>Tanya</div>
-            <div>Histori</div>
-        </div>
-
-        @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
-        @endif
-    </body>
-</html>
+</x-pengguna-layout>
