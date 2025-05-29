d295859173def1babc05cb14bd5a375c3b33cb06
dindputr <dprtia15@gmail.com>
2025-05-29 15:33:26 +0700

    refactor: adjust logo height

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
 
     </head>
     <body >
-        <p class="text-4xl text-blue-500 underline">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <!-- <p class="text-4xl text-blue-500 underline">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
         <div class="bg-red-500 p-8 flex gap-4">
             <div class="bg-green-500 w-3/6">
@@ -24,6 +24,11 @@
             <div class="bg-gray-500">
                 <h1 class="text-2xl font-bold text-white">Welcome to Laravel</h1>
             </div>
+        </div> -->
+
+        <div>
+            <div>Tanya</div>
+            <div>Histori</div>
         </div>
 
         @if (Route::has('login'))
