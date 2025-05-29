2b1b9f3e95c5fe1d9abeb392cda04c9ff9713494
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-05-29 12:41:08 +0700

    feat: penambahan kolom di table pertanyaan part3

--- database/migrations/2025_05_29_050317_create_rules_table.php
+++ database/migrations/2025_05_29_050317_create_rules_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('rules', function (Blueprint $table) {
             $table->id();
-                $table->foreignId('kerusakan_id')->constrained()->onDelete('cascade');
-                $table->foreignId('gejala_id')->constrained()->onDelete('cascade');
+            $table->foreignId('kerusakan_id')->constrained()->onDelete('cascade');
+            $table->foreignId('gejala_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
