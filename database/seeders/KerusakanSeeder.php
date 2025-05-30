7528ebf6215b138616ecab7d3c6271b2d4182308
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-05-30 10:22:18 +0700

    feat: penambahan kerusakan seder

--- database/seeders/KerusakanSeeder.php
+++ database/seeders/KerusakanSeeder.php
@@ -13,7 +13,10 @@ class KerusakanSeeder extends Seeder
      */
     public function run(): void
     {
-        Kerusakan::create(['nama' => 'Kerusakan Baterai', 'kategori' => 'Baterai', 'solusi' => 'Ganti baterai baru.']);
-        Kerusakan::create(['nama' => 'Kerusakan LCD', 'kategori' => 'LCD', 'solusi' => 'Periksa dan ganti LCD.']);
+        Kerusakan::create(['nama' => 'Kerusakan LCD', 'kategori' => 'LCD', 'solusi' => 'Ganti LCD baru.']);
+        Kerusakan::create(['nama' => 'Kerusakan Baterai', 'kategori' => 'Baterai', 'solusi' => 'Ganti batrai baru.']);
+        Kerusakan::create(['nama' => 'Kerusakan Konektor Charger', 'kategori' => 'Konektor Charger', 'solusi' => 'Perbaikan konektor cas, dibersihkan sampahnya, jika tidak bisa diperbaiki maka wajib diganti.']);
+        Kerusakan::create(['nama' => 'Kerusakan Kamera', 'kategori' => 'Kamera', 'solusi' => 'perbaikan kamera dengan cara menambah magnet di dalam bagian kamera, jika tidak bisa di repair maka wajib ganti.']);
+        Kerusakan::create(['nama' => 'Kerusakan Mesin', 'kategori' => 'Mesin', 'solusi' => 'Cek tegangan mesin menggunakan power supply dengan tegangan 3,7 A, jika tegangan tidak stabil maka wajib ganti mesin.']);
     }
 }
