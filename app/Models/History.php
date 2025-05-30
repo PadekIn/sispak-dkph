ebe87a0030c7d4ab7145317f5ae9555fe3f76389
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-05-30 09:48:04 +0700

    feat: penambahan pada model history

--- app/Models/History.php
+++ app/Models/History.php
@@ -9,4 +9,15 @@ class History extends Model
 {
     /** @use HasFactory<\Database\Factories\HistoryFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'kerusakan_id',
+        'gejala_ids',
+        'hasil'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
