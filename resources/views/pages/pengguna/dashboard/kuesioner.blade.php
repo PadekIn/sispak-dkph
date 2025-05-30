5100dbd6475454c0bb044163f2d599dc6df3e1be
dindputr <dprtia15@gmail.com>
2025-05-30 17:49:12 +0700

    feat: update kuesioner page pengguna

--- resources/views/pages/pengguna/dashboard/kuesioner.blade.php
+++ resources/views/pages/pengguna/dashboard/kuesioner.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-pengguna-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Kuesioner') }}
@@ -28,4 +28,4 @@
             </div>
         </div>
     </div>
-</x-guest-layout>
+</x-pengguna-layout>
