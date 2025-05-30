2762e51e6c30733a293f9e250d1541222ae25254
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-05-30 09:47:28 +0700

    feat: penambahan pada model rule

--- app/Models/Rule.php
+++ app/Models/Rule.php
@@ -9,4 +9,16 @@ class Rule extends Model
 {
     /** @use HasFactory<\Database\Factories\RuleFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'kerusakan_id',
+        'gejala_id'
+    ];
+
+    public function kerusakan() {
+        return $this->belongsTo(Kerusakan::class);
+    }
+    public function gejala() {
+        return $this->belongsTo(Gejala::class);
+    }
 }
