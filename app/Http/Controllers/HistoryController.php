392fa351227e8e334a56490a5b7a5fe71912bcee
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-05-30 14:14:32 +0700

    feat: penambahan fitur history

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\History;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
 
 class HistoryController extends Controller
 {
-    //
+    public function indexPengguna() {
+        try {
+            $histories = session('histories', []);
+            return view('history.index', compact('histories'));
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Gagal mengambil data riwayat: ' . $e->getMessage());
+        }
+    }
+
+    public function indexAdmin() {
+        try {
+            $histories = History::with('user')->get();
+            return view('pages.admin.history.index', compact('histories'));
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Gagal mengambil data riwayat: ' . $e->getMessage());
+        }
+    }
+
+
 }
