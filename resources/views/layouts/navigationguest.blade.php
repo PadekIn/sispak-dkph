ba79bca00ad84c22616de47ea81266852f96bdc7
dindputr <dprtia15@gmail.com>
2025-06-01 01:33:36 +0700

    style: adjust style button login and register

--- resources/views/layouts/navigationguest.blade.php
+++ resources/views/layouts/navigationguest.blade.php
@@ -56,9 +56,9 @@
                         </x-dropdown>
                     </div>
                 @else
-                    <div class="space-x-4">
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 hover:text-gray-900">Login</a>
-                        <a href="{{ route('register') }}" class="text-sm text-gray-700 hover:text-gray-900">Register</a>
+                    <div class="flex justify-end gap-2">
+                        <a href="{{ route('register') }}" class="inline-flex items-center mt-4 px-4 py-2 border-gray-800 bg-white border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest hover:bg-gray-200 focus:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Registrasi</a>
+                        <a href="{{ route('login') }}" class="inline-flex items-center mt-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Login</a>
                     </div>
                 @endauth
             </div>
