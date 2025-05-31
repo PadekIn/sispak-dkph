e31fcf25c0f5292f4aebc790eda620c61fce1c43
vtw <vtiaraw2@gmail.com>
2025-05-31 20:24:32 +0700

    refactor: commented sidebar menu's pertanyaan

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -62,14 +62,14 @@
                             <span class="truncate">Gejala</span>
                         </a>
                     </li>
-                    <li>
+                    <!-- <li>
                         <a href="{{ route('admin.pertanyaan.index') }}" class="flex items-center px-4 py-2 text-gray-300 hover:bg-gray-700 rounded-lg {{ request()->routeIs('admin.pertanyaan.*') ? 'bg-gray-700' : '' }}">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
                             </svg>
                             <span class="truncate">Pertanyaan</span>
                         </a>
-                    </li>
+                    </li> -->
                     <li>
                         <a href="{{ route('admin.history.index') }}" class="flex items-center px-4 py-2 text-gray-300 hover:bg-gray-700 rounded-lg {{ request()->routeIs('admin.history.*') ? 'bg-gray-700' : '' }}">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
