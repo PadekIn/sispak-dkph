a64378168054e928b2b4cf2d1ce59ca9560d4525
dindputr <dprtia15@gmail.com>
2025-06-01 07:58:41 +0700

    feat: adjust forgot password layout

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
-<x-guest-layout>
+<x-auth-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Lupa kata sandi? Masukkan email Anda dan kami akan kirimkan tautan untuk mengatur ulang kata sandi.') }}
     </div>
 
     <!-- Session Status -->
@@ -22,4 +22,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-auth-layout>
