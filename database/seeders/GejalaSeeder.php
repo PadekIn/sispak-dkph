6cda93d97829620abca4caa3ffad3b4679fe016e
vtw <vtiaraw2@gmail.com>
2025-06-01 13:37:10 +0700

    refactor: adjust the letter pt.3

--- database/seeders/GejalaSeeder.php
+++ database/seeders/GejalaSeeder.php
@@ -14,18 +14,18 @@ class GejalaSeeder extends Seeder
     public function run(): void
     {
         Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Bergaris']);
-        Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Blank Hitam']);
+        Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Blank hitam']);
         Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Ada tanda-tanda HP hidup, namun layar mati']);
         Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Sebagian LCD bisa disentuh, sebagian lagi tidak bisa disentuh']);
-        Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Bercak Hitam']);
+        Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Bercak hitam']);
         Gejala::create(['kode_gejala' => 'G01', 'nama_gejala' => 'Kurang sensitif atau susah disentuh']);
         Gejala::create(['kode_gejala' => 'G02', 'nama_gejala' => 'Gembung']);
         Gejala::create(['kode_gejala' => 'G02', 'nama_gejala' => 'Dicharger tidak mau penuh 100%']);
         Gejala::create(['kode_gejala' => 'G02', 'nama_gejala' => 'Ketika di charger meloncat dari 20% ke 80%']);
         Gejala::create(['kode_gejala' => 'G02', 'nama_gejala' => 'Baterai 30% tapi tiba-tiba mati']);
-        Gejala::create(['kode_gejala' => 'G02', 'nama_gejala' => 'Main harus sambil di charger, jika tidak di charger mati']);
+        Gejala::create(['kode_gejala' => 'G02', 'nama_gejala' => 'Main harus sambil dicharger, jika tidak dicharger mati']);
         Gejala::create(['kode_gejala' => 'G02', 'nama_gejala' => 'Baterai cepat habis']);
-        Gejala::create(['kode_gejala' => 'G03', 'nama_gejala' => 'HP tidak bisa di charger']);
+        Gejala::create(['kode_gejala' => 'G03', 'nama_gejala' => 'HP tidak bisa dicharger']);
         Gejala::create(['kode_gejala' => 'G03', 'nama_gejala' => 'Kabel USB mesti digoyang baru mau masuk']);
         Gejala::create(['kode_gejala' => 'G03', 'nama_gejala' => 'Dicharger tidak menambahkan daya']);
         Gejala::create(['kode_gejala' => 'G04', 'nama_gejala' => 'Kamera blank hitam']);
