9a707f802aadc893fc79b3ae5f900cdccbc4c27b
vtw <vtiaraw2@gmail.com>
2025-06-01 17:08:37 +0700

    feat: delete admin's menu pertanyaan

--- routes/admin.php
+++ routes/admin.php
@@ -33,15 +33,6 @@ Route::middleware(['web', 'auth', 'isAdmin'])->prefix('admin')->group(function (
         Route::delete('/destroy/{id}', [GejalaController::class, 'destroy'])->name('admin.gejala.destroy');
     });
 
-    Route::prefix('pertanyaan')->group(function(){
-        Route::get('/', [PertanyaanController::class, 'index'])->name('admin.pertanyaan.index');
-        Route::get('/create', [PertanyaanController::class, 'create'])->name('admin.pertanyaan.create');
-        Route::post('/store', [PertanyaanController::class, 'store'])->name('admin.pertanyaan.store');
-        Route::get('/edit/{id}', [PertanyaanController::class, 'edit'])->name('admin.pertanyaan.edit');
-        Route::put('/update/{id}', [PertanyaanController::class, 'update'])->name('admin.pertanyaan.update');
-        Route::delete('/destroy/{id}', [PertanyaanController::class, 'destroy'])->name('admin.pertanyaan.destroy');
-    });
-
     Route::prefix('history')->group(function(){
         Route::get('/', [HistoryController::class, 'indexAdmin'])->name('admin.history.index');
     });
