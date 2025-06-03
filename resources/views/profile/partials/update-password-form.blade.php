05c432ce617dbdcb4a2d7106e55aa4627979b115
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2025-06-03 17:01:29 +0700

    fix: fixing some bugs

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -45,7 +45,7 @@
                     <svg class="inline w-4 h-4 mr-2 text-green-500" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
                     </svg>
-                    {{ __('Tersimpan') }}
+                    {{ __('Kata sandi Anda berhasil diperbarui.') }}
                 </p>
             @endif
         </div>
