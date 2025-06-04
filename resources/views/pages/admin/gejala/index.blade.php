28d77b42b5923aa2ff0bce6aeca3eae5916154fb
vtw <vtiaraw2@gmail.com>
2025-06-04 23:32:43 +0700

    chore: just adding an horizontal line inside the table pt.2

--- resources/views/pages/admin/gejala/index.blade.php
+++ resources/views/pages/admin/gejala/index.blade.php
@@ -43,9 +43,9 @@
                             <tbody class="bg-white divide-y divide-gray-200">
                                 @forelse ($gejalas as $gejala)
                                     <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $gejala->kode_gejala }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $gejala->nama_gejala }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-b-2 border-gray-200">{{ $gejala->kode_gejala }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-b-2 border-gray-200">{{ $gejala->nama_gejala }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium border-b-2 border-gray-200">
                                             <div class="flex justify gap-3">
                                                 <a href="{{ route('admin.gejala.edit', $gejala->id) }}" class="text-indigo-600 hover:text-indigo-900">Ubah</a>
                                                 <button type="button"
