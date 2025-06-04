4e43bb9b2af920c7a8a034ce579f70c54e2d2f14
dindputr <dprtia15@gmail.com>
2025-06-04 21:55:51 +0700

    chore: change label Histori Diagnosa to Histori

--- resources/views/pages/pengguna/history/index.blade.php
+++ resources/views/pages/pengguna/history/index.blade.php
@@ -1,7 +1,7 @@
 <x-pengguna-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Histori Diagnosa') }}
+            {{ __('Histori') }}
         </h2>
     </x-slot>
 
