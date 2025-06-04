9b2ab0b4e0382700c29d509c9b9fcb854470feb2
vtw <vtiaraw2@gmail.com>
2025-06-04 23:32:54 +0700

    chore: just adding an horizontal line inside the table pt.3

--- resources/views/pages/admin/kerusakan/index.blade.php
+++ resources/views/pages/admin/kerusakan/index.blade.php
@@ -43,9 +43,9 @@
                             <tbody class="bg-white divide-y divide-gray-200">
                                 @forelse ($kerusakans as $kerusakan)
                                     <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $kerusakan->jenis_kerusakan }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $kerusakan->solusi }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-b-2 border-gray-200">{{ $kerusakan->jenis_kerusakan }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 border-b-2 border-gray-200">{{ $kerusakan->solusi }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium border-b-2 border-gray-200">
                                             <div class="flex justify gap-3">
                                                 <a href="{{ route('admin.kerusakan.edit', $kerusakan->id) }}" class="text-indigo-600 hover:text-indigo-900">Ubah</a>
                                                 <button type="button"
